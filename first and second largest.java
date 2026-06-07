c97b254e666a03fac7fbfb3d63eb1c345e03867a
Logeswari2007 <llog.cse2024@rmd.ac.in>
2026-06-07 10:28:52 +0530

    Add files via upload

---
+++ first and second largest.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int firstLargest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > firstLargest) {
+                secondLargest = firstLargest;
+                firstLargest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != firstLargest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("First Largest = " + firstLargest);
+        System.out.println("Second Largest = " + secondLargest);
+    }
+}
